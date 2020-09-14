39ad88097bea9fcec839a3943da38b525becfa65
vivi.peng <vivi.peng@aplus-digital.com>
2020-09-14 17:08:05 +0800

    新增題目 修正題目js

--- js/exam.js
+++ js/exam.js
@@ -1,9 +1,63 @@
 var question =[
 	{
+		title: '細潔無隱角EX抗敏護齦牙刷，每根刷毛尖端處又再分岔成幾根刷毛？',
+		answer: [
+			{
+				text: '2根',
+				correct: false
+			},
+			{
+				text: '3根',
+				correct: true
+			},
+			{
+				text: '沒分岔1根到底',
+				correct: false
+			}
+		],
+		conclusion: '能屈能伸軟實力大丈夫<br>舒緩牙齦壓力超級有淨'
+	},
+	{
+		title: '「護齦神盾」適齦佳修護PLUS牙膏的三大護齦保養配方能提供什麼幫助？',
+		answer: [
+			{
+				text: '降低牙周病發生率',
+				correct: false
+			},
+			{
+				text: '幫助預防牙齦流血問題',
+				correct: false
+			},
+			{
+				text: '減少細菌滋生率',
+				correct: false
+			},
+			{
+				text: '以上皆是',
+				correct: true
+			}
+		],
+		conclusion: '牙刷月更換偵查精兵<br>關心口腔健康真愛認證'
+	},
+	{
+		title: '漱口水越辣越有效嗎？',
+		answer: [
+			{
+				text: '是',
+				correct: false
+			},
+			{
+				text: '否',
+				correct: true
+			}
+		],
+		conclusion: '溫和抗敵新一代宗師<br>以柔克剛消滅牙菌斑'
+	},
+	{
 		title: '牙刷應多久更換一支才健康？',
 		answer: [
 			{
-				text: '2週',
+				text: '2周',
 				correct: false
 			},
 			{
@@ -15,151 +69,114 @@ var question =[
 				correct: false
 			}
 		],
-		conclusion: ['牙刷月更換偵查精兵','關心口腔健康真愛認證']
+		conclusion: '牙刷月更換偵查精兵<br>關心口腔健康真愛認證'
 	},
 	{
-		title: '題目2',
+		title: '牙膏和家人共用是否正確？',
 		answer: [
 			{
-				text: '我是對的',
+				text: '是',
+				correct: false
+			},
+			{
+				text: '否',
+				correct: true
+			}
+		],
+		conclusion: '牙刷月更換偵查精兵<br>關心口腔健康真愛認證'
+	},
+	{
+		title: '以下哪項好習慣能防止病從口入？',
+		answer: [
+			{
+				text: '共用餐具',
+				correct: false
+			},
+			{
+				text: '使用公筷母匙',
 				correct: true
 			},
 			{
-				text: '我是錯的',
-				correct: false
-			},
-			{
-				text: '我是錯的',
+				text: '咳嗽不遮掩',
 				correct: false
 			}
 		],
-		conclusion: ['牙刷月更換偵查精兵','關心口腔健康真愛認證']
+		conclusion: '月圓人團圓聚餐公筷母匙<br>朋友圈防疫護齦小達人'
 	},
 	{
-		title: '題目3',
+		title: '如何挑選與使用護齦漱口水?',
 		answer: [
 			{
-				text: '錯',
+				text: '共用餐具',
 				correct: false
 			},
 			{
-				text: '對',
+				text: '使用公筷母匙',
 				correct: true
 			},
 			{
-				text: '錯',
+				text: '咳嗽不遮掩',
 				correct: false
 			}
 		],
-		conclusion: ['牙刷月更換偵查精兵','關心口腔健康真愛認證']
-	},
-	{
-		title: '題目4',
-		answer: [
-			{
-				text: '答案一',
-				correct: true
-			},
-			{
-				text: '答案二',
-				correct: false
-			},
-			{
-				text: '答案三',
-				correct: false
-			}
-		],
-		conclusion: ['牙刷月更換偵查精兵','關心口腔健康真愛認證']
-	},
-	{
-		title: '題目5',
-		answer: [
-			{
-				text: '答案一',
-				correct: true
-			},
-			{
-				text: '答案二',
-				correct: false
-			},
-			{
-				text: '答案三',
-				correct: false
-			}
-		],
-		conclusion: ['牙刷月更換偵查精兵','關心口腔健康真愛認證']
-	},
-	{
-		title: '題目6',
-		answer: [
-			{
-				text: '答案一',
-				correct: true
-			},
-			{
-				text: '答案二',
-				correct: false
-			},
-			{
-				text: '答案三',
-				correct: false
-			}
-		],
-		conclusion: ['牙刷月更換偵查精兵','關心口腔健康真愛認證']
+		conclusion: '月圓人團圓聚餐公筷母匙<br>朋友圈防疫護齦小達人'
 	}
 ];
 var qNum = 0;
 function enterText(qNum){
 	$('.question .text').text(question[qNum].title);
+	$('.answers').empty();
 	for (var i=0; i<question[qNum].answer.length; i++) {
-		$('.answer .text').eq(i).text(question[qNum].answer[i].text);
+		// $('.answer .text').eq(i).text(question[qNum].answer[i].text);
+		$('.answers').append('<li class="answer" data-answer="'+i+'">'+question[qNum].answer[i].text+'</li>');
 	}
 }
-enterText(qNum);
+
 
 $('.start').click(function (e) { 
 	e.preventDefault();
-	$('.examItem').css('display','none');
-	$('.examQA').css('display','block');
+	enterText(qNum);
+	$('.examItem').fadeOut();
+	$('.examQA').fadeIn();
 });
 
-$('.answer').click(function (e) { 
+$('.answers').click(function (e) { 
 	e.preventDefault();
-	var aNum = $(this).data('answer');
-	$('.examItem').css('display','none');
+	var aNum = $('.answer').data('answer');
+	$('.examItem,.resultItem').fadeOut();
 	$('.examResult').css('display','block');
 	if (question[qNum].answer[aNum].correct == true){
 		console.log('成功');
-		$('.correct .content').text(question[qNum].conclusion);
-		// $('.examResult .correct').css('display','block');
+		$('.correct .content .main').html(question[qNum].conclusion);
+		$('.resultItem.correct').fadeIn();
 	} else if (qNum == question.length - 1){
 		console.log('全失敗');
-		// $('.examResult .allWrong').css('display','block');
+		$('.resultItem.allWrong').fadeIn();
 	} else {
 		console.log('失敗');
 		qNum = qNum+1;
-		// $('.examResult .wrong').css('display','block');
+		$('.resultItem.wrong').fadeIn();
 	}
 });
 
 $('.getCoupon').click(function (e) { 
 	e.preventDefault();
-	$('.examItem').css('display','none');
-	$('.examCoupon').css('display','block');
+	$('.examItem').fadeOut();
+	$('.examCoupon').fadeIn();
 });
 
 $('.nextQ').click(function (e) { 
 	e.preventDefault();
 	enterText(qNum);
-	$('.examItem').css('display','none');
-	$('.examQA').css('display','block');
+	$('.examItem').fadeOut();
+	$('.examQA').fadeIn();
 });
 
 $('.tryAgain').click(function (e) { 
 	e.preventDefault();
 	qNum = 0;
-	$('.examItem').css('display','none');
-	$('.examIndex').css('display','block');
+	$('.examItem').fadeOut();
+	$('.examIndex').fadeIn();
 });
 
 var today = new Date(),
