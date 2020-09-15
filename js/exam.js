3bd024e6b0234db034451059e36a1395ac1070eb
彭立璇 <kunbu0203@gmail.com>
2020-09-16 03:07:09 +0800

    fix

--- js/exam.js
+++ js/exam.js
@@ -262,7 +262,7 @@ $(document).ready(function () {
 		e.preventDefault();
 		qNum = 0;
 		$('.examIndex').fadeIn();
-		$('.visits .number').addClass('animate');
+		$('.visits .number').addClass('animate').html('0<br>1<br>2<br>3<br>4<br>5<br>6<br>7<br>8<br>9<br>0');
 		setTimeout(function() {
 			setNum();
 		}, 1000);
