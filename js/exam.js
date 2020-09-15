cd075b20b165a0eb989f35f2deddf961694ccbac
彭立璇 <kunbu0203@gmail.com>
2020-09-16 02:56:07 +0800

    fix

--- js/exam.js
+++ js/exam.js
@@ -262,7 +262,14 @@ $(document).ready(function () {
 		e.preventDefault();
 		qNum = 0;
 		$('.examIndex').fadeIn();
+		today = new Date(),
+		milliseconds = today.getTime(),
+		startDate = new Date(2020, (9-1), 5);
+		visits = parseInt((today - startDate)/300000+500);
+		visitsStr = visits.toString();
+		strLength = visitsStr.length;
 		setNum();
+		alert('重新');
 	});
 
 	// setInterval(function(){
